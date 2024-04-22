dc33460b939ef384839b82b6112fbf3df295d36f
LAPTOP-R91CRERV\84869 <duong.nguyenwt16@hcmut.edu.vn>
2024-04-22 21:23:58 +0700

    Update split file function

--- Split file into chunks/split_to_chunk.py
+++ Split file into chunks/split_to_chunk.py
@@ -12,11 +12,11 @@ def file_split(file_path: str, chunk_size: int):
         except FileNotFoundError:
             print(f"Can not find: {file_path}")
 
-    def save_chunks_to_directory(chunks: list, output_directory: str, file_name: str):
+    def save_chunks_to_directory(chunks: list, output_directory: str, file_name: str, file_exten: str):
         try:
             os.makedirs(output_directory, exist_ok=True)  # Tạo thư mục nếu chưa tồn tại
             for i, chunk in enumerate(chunks):
-                output_file = os.path.join(output_directory, f"{file_name}_chunk_{i}.bin")
+                output_file = os.path.join(output_directory, f"{file_name}_{file_exten}_{i}.bin")
                 with open(output_file, "wb") as out_f:
                     out_f.write(chunk)
                 print(f"Chunk created {i}: {output_file}")
@@ -24,10 +24,12 @@ def file_split(file_path: str, chunk_size: int):
             print(f"Chunks saving error {e}")
 
     # Get the file name without extension
-    file_name = os.path.splitext(os.path.basename(file_path))[0] #Take input file's name
+    file_name, file_exten = os.path.splitext(os.path.basename(file_path)) #Take input file's name and extension
+    file_name = file_name.replace('.', '_')  # Replace '.' in file name with '_'
+    file_exten = file_exten.replace('.', '')  # Remove '.' from file extension
 
     chunks = list(split_file_to_chunks(file_path, chunk_size))
-    save_chunks_to_directory(chunks, "output chunk file", file_name)
+    save_chunks_to_directory(chunks, f"{file_name}_{file_exten}", file_name, file_exten)
 
     return len(chunks)  # Return the number of chunks
 
